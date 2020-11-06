73891146e37b9a6ca91ef4dd96bbd2760e88c1ed
César <c.caycho46@gmail.com>
2020-11-05 19:06:45 -0500

    primer avance de la funcion main

--- matematicas.go
+++ matematicas.go
@@ -8,7 +8,18 @@ import (
 
 func main() {
 
+	var numerooperaciones int
+
 	fmt.Println("Bienvenido")
+
+	for numerooperaciones < 1 {
+
+		fmt.Println("¿Cuantas operaciones deseas realizar")
+		fmt.Scan(&numerooperaciones)
+		if numerooperaciones < 1 {
+			fmt.Println("Hey ese no es un numero valido >:v")
+		}
+	}
 
 	fmt.Println(mate.Suma(1, 2))
 
