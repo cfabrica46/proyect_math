24741f5b37c41294d7370243c7334a489fa8e390
César <c.caycho46@gmail.com>
2020-11-06 18:43:59 -0500

    cree un mapa para almacenar datos

--- matematicas.go
+++ matematicas.go
@@ -10,6 +10,16 @@ func main() {
 
 	var numerooperaciones int
 
+	operaciones := make(map[string]int, 4)
+	operaciones["suma"] = 0
+	operaciones["resta"] = 0
+	operaciones["multiplicación"] = 0
+	operaciones["división"] = 0
+
+	fmt.Println(operaciones)
+	return
+
+	//Introducción...
 	fmt.Println("Bienvenido")
 
 	for numerooperaciones < 1 {
@@ -21,6 +31,13 @@ func main() {
 		}
 	}
 
-	fmt.Println(mate.Suma(1, 2))
+	fmt.Println("Muy bien")
 
+	fmt.Println("¿Que ejercicios deseas realizar?")
+
+	for i := 0; i < len(operaciones); i++ {
+		fmt.Printf("%d.%s")
+	}
+
+	fmt.Println(mate.Suma(1, 2))
 }
