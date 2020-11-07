c53358d3a6ce354c310c443be45abdfd097e2344
César <c.caycho46@gmail.com>
2020-11-06 19:34:41 -0500

    termine el algoritmo de eleeccion de operacion

--- matematicas.go
+++ matematicas.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	var numerooperaciones int
+	var numerooperaciones, eleccionoperaciones int
 
 	vector1 := []string{"suma", "0"}
 	vector2 := []string{"resta", "0"}
@@ -22,7 +22,7 @@ func main() {
 
 	for numerooperaciones < 1 {
 
-		fmt.Println("¿Cuantas operaciones deseas realizar")
+		fmt.Println("¿Cuantas operaciones deseas realizar?")
 		fmt.Scan(&numerooperaciones)
 		if numerooperaciones < 1 {
 			fmt.Println("Hey ese no es un numero valido >:v")
@@ -36,7 +36,25 @@ func main() {
 	for i := 0; i < len(operaciones); i++ {
 		fmt.Printf("%d.%s\t", i+1, operaciones[i][0])
 	}
-	fmt.Println("")
+	fmt.Println("0.Listo")
+
+	for {
+		fmt.Scan(&eleccionoperaciones)
+
+		if eleccionoperaciones == 0 {
+			break
+		}
+
+		if eleccionoperaciones <= len(operaciones) {
+
+			operaciones[eleccionoperaciones-1][1] = "1"
+		} else {
+			fmt.Println("Hey, esa no era una opción")
+		}
+
+	}
+
+	fmt.Println(operaciones)
 
 	fmt.Println(mate.Suma(1, 2))
 }
